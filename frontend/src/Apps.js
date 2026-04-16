7341b9e778abf8fb39db94a24fd41baf3c240082
Siva Sindhuja Tsundupalli <160160724+Sivasindhuja@users.noreply.github.com>
2026-04-17 00:54:21 +0530

    Create Apps.js to render Feedback and Complaint forms

---
+++ frontend/src/Apps.js
@@ -0,0 +1,13 @@
+import FeedbackForm from './FeedbackForm';
+import ComplaintForm from './ComplaintForm';
+
+function App() {
+  return (
+    <>
+      <FeedbackForm />
+      <ComplaintForm />
+    </>
+  );
+}
+
+export default App;
